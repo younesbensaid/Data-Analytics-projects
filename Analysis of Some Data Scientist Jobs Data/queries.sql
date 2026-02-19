da1f2a7c296cc7989edeb9ddcfc4bf5353ef5978
Younes Bensaid <younesbensa3id@gmail.com>
2026-02-19 21:46:43 +0100

    create a new table in the data base

---
+++ Analysis of Some Data Scientist Jobs Data/queries.sql
@@ -0,0 +1,19 @@
+
+---- Create Some_data_science_jobs_data table
+-------------------------------------------------------------
+CREATE TABLE Some_data_science_jobs_data (
+    work_year INT,
+    experience_level TEXT,
+    employment_type TEXT,
+    job_title TEXT,
+    salary  NUMERIC,
+    salary_currency TEXT,
+    salary_in_usd NUMERIC,
+    employee_residence TEXT,
+    remote_ratio NUMERIC,
+    company_location TEXT,
+    company_size TEXT
+);
+
+--------------------------------------------------------------
+
