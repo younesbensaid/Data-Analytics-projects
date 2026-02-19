5e2e930e283760daa90d59baf8737fe71a4860c8
Younes Bensaid <younesbensa3id@gmail.com>
2026-02-20 00:15:06 +0100

    filter jobs

--- Analysis of Some Data Scientist Jobs Data/Data Analysis & Business Intelligence Roles Tracking.sql
+++ Analysis of Some Data Scientist Jobs Data/Data Analysis & Business Intelligence Roles Tracking.sql
@@ -3,5 +3,34 @@
 -- intelligence (BI), and analytics. It includes positions such as Data 
 -- Analysts, Analytics Engineers, and BI Developers. 
 
+SELECT *
+FROM some_data_science_jobs_data
+WHERE job_title IN (
+    'Analytics Engineer',
+    'BI Analyst',
+    'BI Data Analyst',
+    'BI Developer',
+    'Business Data Analyst',
+    'Business Intelligence Engineer',
+    'Compliance Data Analyst',
+    'Data Analyst',
+    'Data Analytics Consultant',
+    'Data Analytics Engineer',
+    'Data Analytics Lead',
+    'Data Analytics Manager',
+    'Data Analytics Specialist',
+    'Data Operations Analyst',
+    'Data Quality Analyst',
+    'Finance Data Analyst',
+    'Financial Data Analyst',
+    'Insight Analyst',
+    'Lead Data Analyst',
+    'Marketing Data Analyst',
+    'Power BI Developer',
+    'Principal Data Analyst',
+    'Product Data Analyst',
+    'Staff Data Analyst'
+);
+
 
 --------------------------------------------------------------------------------------------
