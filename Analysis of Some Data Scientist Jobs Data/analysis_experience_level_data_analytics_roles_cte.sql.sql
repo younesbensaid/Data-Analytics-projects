75412cb8cddc34365a6d1646a85dc4823a872e9e
Younes Bensaid <younesbensa3id@gmail.com>
2026-02-20 15:42:43 +0100

    add an introduction about analysis exprience level for data analytics roles

---
+++ Analysis of Some Data Scientist Jobs Data/analysis_experience_level_data_analytics_roles_cte.sql.sql
@@ -0,0 +1,22 @@
+/*
+File: analysis_experience_level_data_analytics_roles_cte.sql
+
+
+NOTE:
+The experience levels distribution presented in this analysis reflects
+only the data available in the current dataset I am working with. This 
+distribution is not intended to be an accurate or comprehensive 
+representation of the actual job market or the true distribution of 
+experience levels in analytics roles in general.
+
+
+Description:
+This query analyzes the distribution of experience levels
+for selected Data Analysis and Business Intelligence roles.
+
+Purpose:
+To evaluate how experience levels (EN, MI, SE, EX)
+are distributed across the data we are working in
+
+
+*/
