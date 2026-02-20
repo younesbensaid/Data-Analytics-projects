d0d724d40d25a5de32630ef6b07f2bfa989683c5
Younes Bensaid <younesbensa3id@gmail.com>
2026-02-20 18:22:44 +0100

    add an introduction to analysis-salaries-for-data-analytics-roles.sql

---
+++ Analysis of Some Data Scientist Jobs Data/analysis-salaries-for-data-analytics-roles.sql
@@ -0,0 +1,20 @@
+/*
+File: analysis-salaries-for-data-analytics-roles.sql
+
+
+NOTE:
+The salaries distribution presented in this analysis reflects
+only the data available in the current dataset I am working with. This
+distribution is not necessarily an accurate or comprehensive
+representation of the actual labor market or the true distribution of
+salaries in analytics roles in general. 
+
+
+Description:
+This queries analyzes the salaries in data analytics roles
+
+Purpose:
+To have an overview about the salaries
+across the data we are working in
+
+*/
